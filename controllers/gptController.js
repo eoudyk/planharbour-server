65bdab6ac5b393fa30342ef51b5682af99d5a595
Emily Oudyk <eoudyk@gmail.com>
2023-10-17 14:05:08 -0400

    add further functionality so will fetch response without errors each time

--- controllers/gptController.js
+++ controllers/gptController.js
@@ -1,6 +1,7 @@
-// controllers/gptController.js
-
 const axios = require("axios");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const path = require("path");
 
 exports.sendMessageToGPT = async (req, res) => {
   const userMessage = req.body.prompt;
@@ -11,7 +12,11 @@ exports.sendMessageToGPT = async (req, res) => {
       {
         model: "gpt-3.5-turbo",
         messages: [
-          { role: "system", content: "You are a helpful assistant." },
+          {
+            role: "system",
+            content:
+              "You are a helpful assistant creating a supply teacher plan.",
+          },
           { role: "user", content: userMessage },
         ],
       },
@@ -23,9 +28,38 @@ exports.sendMessageToGPT = async (req, res) => {
     );
 
     const gptResponse = response.data.choices[0]?.message?.content.trim();
+
     res.json({ message: gptResponse });
   } catch (error) {
-    console.error("Error:", error.response.data);
+    console.error("Error:", error.response?.data || error.message);
     res.status(500).json({ error: "Failed to communicate with OpenAI" });
   }
 };
+
+//temporary save file:
+// exports.saveLessonToFile = (req, res) => {
+//   const { title, content } = req.body;
+
+//   if (!content) {
+//     return res.status(400).json({ error: "Content is required." });
+//   }
+
+//   const lessonsFilePath = path.join(__dirname, "lessons.json");
+
+//   let lessons = [];
+
+//   if (fs.existsSync(lessonsFilePath)) {
+//     lessons = JSON.parse(fs.readFileSync(lessonsFilePath, "utf-8"));
+//   }
+
+//   const newLesson = {
+//     id: uuidv4(),
+//     content,
+//   };
+
+//   lessons.push(newLesson);
+
+//   fs.writeFileSync(lessonsFilePath, JSON.stringify(lessons, null, 2));
+
+//   res.json(newLesson);
+// };
--- routes/gptRoutes.js
+++ routes/gptRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const gptController = require("../controllers/gptController");
 
 router.post("/sendToGPT", gptController.sendMessageToGPT);
+// router.post("/saveLessonToFile", gptController.saveLessonToFile);
 
 module.exports = router;
